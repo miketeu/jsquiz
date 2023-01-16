2d772d5567fdcccf507b552708977dea128b9d26
Mike Teuten <mike.teuten@gmail.com>
2023-01-16 14:01:01 +0000

    Added code to make Questions appear. Just heading at the moment- more work to get questions to show.

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,7 +1,15 @@
 //Create a code quiz that contains the following requirements:
 
 //* A start button that when clicked a timer starts and the first question appears.
+document.querySelector("#start").addEventListener("click", function () {
+  document.querySelector("#questions").style.display = "block";
+  document.querySelector("#start-screen").style.display = "none";
+});
 
+document.querySelector("#questions").addEventListener("click", function () {
+  document.querySelector("#start-screen").style.display = "block";
+  document.querySelector("#questions").style.display = "none";
+});
 //Timer
 
 
