ddd1c136e764dc9b03878aea23fe6a6d717da9a2
Mike Teuten <mike.teuten@gmail.com>
2023-01-17 11:40:28 +0000

    add timer

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -6,10 +6,33 @@ document.querySelector("#start").addEventListener("click", function () {
   document.querySelector("#start-screen").style.display = "none";
 });
 
+
+
+let choicesEl = document.querySelector("#choices");
+let question = [
+  { question: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2 },
+
+  { question: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2 },
+];
+
+let currentQuestion = 1;
+
+choicesEl.addEventListener("click", function (event) {
+  if (event.target.matches("button")) {
+    console.log(event.target.getAttribute("data-index"));
+  }
+});
+
+
+
+
+/*
 document.querySelector("#questions").addEventListener("click", function () {
   document.querySelector("#start-screen").style.display = "block";
   document.querySelector("#questions").style.display = "none";
 });
+*/
+
 //Timer
 
 
--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,5 +1,29 @@
 //*Create a code quiz that contains the following requirements:
 
+// The timer
+Window.onload = startTimer();
+
+function startTimer(duration, display) {
+  var time = duration, minutes, seconds;
+  setInterval(function() {
+    minutes = parseInt(time / 60, 10);
+    seconds = parseInt(time % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds: seconds;
+    if (--time < 0) {
+      time = 0;
+      // To do auto reset of timer 
+
+    time = duration;
+    }
+
+  }, 1000);
+}
+
+
+
+
 var theQuestions = [
   {
     question: "Inside which HTML element do we put the JavaScript?",
@@ -14,7 +38,11 @@ var theQuestions = [
 
 
 
+
+
 // * When answer is clicked, the next question appears
+
+
 
 let choicesEl = document.querySelector("#choices");
 let question = [
@@ -31,7 +59,7 @@ choicesEl.addEventListener("click", function (event) {
   }
 });
 
-// * If the answer clicked was incorrect then subtract time from the clock
 
+// * If the answer clicked was incorrect then subtract time from the clock
 //* The quiz should end when all questions are answered or the timer reaches 0.
 
