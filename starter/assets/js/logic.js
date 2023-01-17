7794be92899f4a2feaeed8bb792c18d29dc60d60
Mike Teuten <mike.teuten@gmail.com>
2023-01-17 13:10:15 +0000

    Changed timer. Now working but needs modification to action 0 time status. (make sound and shut down quiz and update score etc.)

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,18 +1,62 @@
 //Create a code quiz that contains the following requirements:
 
 //* A start button that when clicked a timer starts and the first question appears.
+
 document.querySelector("#start").addEventListener("click", function () {
   document.querySelector("#questions").style.display = "block";
   document.querySelector("#start-screen").style.display = "none";
+
+  //Get the timer
+  document.getElementById("time").innerHTML = 01 + ":" + 11;
+  startTimer();
+
+  //Get questions
 });
 
+//Timer starts here
 
+function startTimer() {
+  var presentTime = document.getElementById("time").innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond(timeArray[1] - 1);
+  if (s == 59) {
+    m = m - 1;
+  }
+  if (m < 0) {
+    return;
+  }
+
+  document.getElementById("time").innerHTML = m + ":" + s;
+  console.log(m);
+  setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {
+    sec = "0" + sec;
+  } // add zero in front of numbers < 10
+  if (sec < 0) {
+    sec = "59";
+  }
+  return sec;
+}
+
+//End timer.
 
 let choicesEl = document.querySelector("#choices");
-let question = [
-  { question: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2 },
+let questions = [
+  {
+    question: "This is question 1. Is it :-",
+    answers: ["ex1", "ex2", "ex3", "ex4"],
+    correctAnswer: 0,
+  },
 
-  { question: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2 },
+  {
+    question: "Q-2  what is answer?",
+    answers: ["ex1-2", "ex2-2", "ex3-2", "ex4-2"],
+    correctAnswer: 3,
+  },
 ];
 
 let currentQuestion = 1;
@@ -23,9 +67,6 @@ choicesEl.addEventListener("click", function (event) {
   }
 });
 
-
-
-
 /*
 document.querySelector("#questions").addEventListener("click", function () {
   document.querySelector("#start-screen").style.display = "block";
@@ -34,5 +75,3 @@ document.querySelector("#questions").addEventListener("click", function () {
 */
 
 //Timer
-
-
--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,63 +1,10 @@
 //*Create a code quiz that contains the following requirements:
 
-// The timer
-Window.onload = startTimer();
-
-function startTimer(duration, display) {
-  var time = duration, minutes, seconds;
-  setInterval(function() {
-    minutes = parseInt(time / 60, 10);
-    seconds = parseInt(time % 60, 10);
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds: seconds;
-    if (--time < 0) {
-      time = 0;
-      // To do auto reset of timer 
-
-    time = duration;
-    }
-
-  }, 1000);
-}
-
-
-
-
-var theQuestions = [
-  {
-    question: "Inside which HTML element do we put the JavaScript?",
-
-    answers: {
-
-    }
-
-  },
-];
-
-
 
 
 
 
 // * When answer is clicked, the next question appears
-
-
-
-let choicesEl = document.querySelector("#choices");
-let question = [
-  { question: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2 },
-
-  { question: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2 },
-];
-
-let currentQuestion = 1;
-
-choicesEl.addEventListener("click", function (event) {
-  if (event.target.matches("button")) {
-    console.log(event.target.getAttribute("data-index"));
-  }
-});
 
 
 // * If the answer clicked was incorrect then subtract time from the clock
